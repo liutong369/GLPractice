7f8642c91cd491c8d034f0f02e759c341a5c26c6
liutong369 <liutong369@foxmail.com>
2024-05-01 01:36:53 +0800

    first commit

---
+++ open_source/readme.md
@@ -0,0 +1,16 @@
+# open source
+
+need `glfw` and `glad` at least
+
+1. `glfw`  is cloned from [glfw/glfw: A multi-platform library for OpenGL, OpenGL ES, Vulkan, window and input (github.com)](https://github.com/glfw/glfw) with tag 3.4
+
+2. `glad` download from [glad source download online](https://glad.dav1d.de/) with the following options:
+
+| option        | value             |
+| ------------- | ----------------- |
+| Language      | C/C++             |
+| Specification | OpenGL            |
+| API.gl        | Version 4.6       |
+| Profile       | Core              |
+| Extensions    | None              |
+| Options       | Generate a loader |
---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+#include <wtypes.h>
+#include <iostream>
+#include <thread>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+using namespace std;
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    cout << "glfw version: " << glfwGetVersionString() << endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__ // macos
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(1280, 960, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+	const GLubyte* name = glGetString(GL_VENDOR); //返回负责当前OpenGL实现厂商的名字
+	const GLubyte* biaoshifu = glGetString(GL_RENDERER); //返回一个渲染器标识符，通常是个硬件平台
+	const GLubyte* OpenGLVersion = glGetString(GL_VERSION); //返回当前OpenGL实现的版本号
+ 
+	printf("OpenGL实现厂商的名字：%s\n", name);
+	printf("渲染器标识符：%s\n", biaoshifu);
+	printf("OpenGL实现的版本号：%s\n", OpenGLVersion);
+	
+
+    while(!glfwWindowShouldClose(window))
+    {
+        // 输入
+        processInput(window);
+
+        // 渲染 render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    cout << "hello kitty" << endl;
+
+    return 0;
+}
